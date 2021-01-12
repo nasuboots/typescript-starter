82132e403bd62bbcce22c8ffdf81232116ab2492
nasuboots <50765809+nasuboots@users.noreply.github.com>
2021-01-12 14:46:06 +0000

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,19 @@
 /** @typedef {import('@typescript-eslint/parser').ParserOptions} ParserOptions */
 /** @typedef {ESLintConfig & { parserOptions: ParserOptions }} Config */
 
-module.exports = /** @type {Config} */({
+module.exports = /** @type {Config} */ ({
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.dev.json'],
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+    'prettier/@typescript-eslint',
   ],
 })
