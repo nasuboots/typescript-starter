09c5d0beb33db83b038eab01eb0d75ebbb105332
nasuboots <50765809+nasuboots@users.noreply.github.com>
2021-01-12 14:20:12 +0000

    lint with type checking

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,20 @@
-/** @typedef {import('eslint').Linter.Config} Config */
+/** @typedef {import('eslint').Linter.Config} ESLintConfig */
+/** @typedef {import('@typescript-eslint/parser').ParserOptions} ParserOptions */
+/** @typedef {ESLintConfig & { parserOptions: ParserOptions }} Config */
 
 module.exports = /** @type {Config} */({
   root: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.dev.json'],
+  },
   plugins: [
     '@typescript-eslint',
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
 })
