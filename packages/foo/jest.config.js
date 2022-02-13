a3609ddc2235cf79df709260c285c2a6fc85f67f
nasuboots <50765809+nasuboots@users.noreply.github.com>
2022-02-13 13:20:24 +0000

    allow comments in tsconfig

--- packages/foo/jest.config.js
+++ packages/foo/jest.config.js
@@ -1,8 +1,15 @@
 /** @typedef {import('@jest/types').Config.InitialOptions} Config */
 
+const fs = require('fs')
+const path = require('path')
+
+const json5 = require('json5')
 const { pathsToModuleNameMapper } = require('ts-jest')
 
-const { compilerOptions } = require('../../tsconfig.base.json')
+/** @type {import('../../tsconfig.base.json')} */
+const { compilerOptions } = json5.parse(
+  fs.readFileSync(path.resolve(__dirname, '../../tsconfig.base.json'), 'utf-8')
+)
 
 /** @type {Config} */
 const config = {
