96dd02aa521867baae8ad64a0c0b9b620bc6eb37
nasuboots <50765809+nasuboots@users.noreply.github.com>
2022-02-13 13:53:28 +0000

    add build scripts

---
+++ packages/foo/scripts/build.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+const esbuild = require('esbuild')
+
+esbuild
+  .build({
+    entryPoints: [path.resolve(__dirname, '../src/index.ts')],
+    outdir: path.resolve(__dirname, '../../../dist/packages/foo'),
+    bundle: true,
+    minify: true,
+    platform: 'node',
+    target: 'node16',
+    tsconfig: path.resolve(__dirname, '../tsconfig.app.json'),
+    mainFields: ['module', 'main'],
+  })
+  .catch((err) => {
+    console.error(err)
+    // eslint-disable-next-line no-process-exit
+    process.exit(1)
+  })
